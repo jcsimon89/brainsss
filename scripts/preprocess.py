2b11d080ab0c40382caf3b63213493b896e1965c
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2023-10-25 16:45:17 -0700

    update slurm time memory limits

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -374,7 +374,7 @@ def main(args):
                                  script=os.path.join(scripts_path, script),
                                  modules=modules,
                                  args=args,
-                                 logfile=logfile, time=1, mem=2, nice=nice, nodes=nodes)
+                                 logfile=logfile, time=4, mem=4, nice=nice, nodes=nodes)
             brainsss.wait_for_job(job_id, logfile, com_path)
 
     if highpass:
@@ -394,7 +394,7 @@ def main(args):
                                  script=os.path.join(scripts_path, script),
                                  modules=modules,
                                  args=args,
-                                 logfile=logfile, time=4, mem=2, nice=nice, nodes=nodes)
+                                 logfile=logfile, time=4, mem=4, nice=nice, nodes=nodes)
             brainsss.wait_for_job(job_id, logfile, com_path)
 
     if correlation:
