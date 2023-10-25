bff79017d32654253307a64ef37de72af0dae278
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2023-10-25 11:59:36 -0700

    Update preprocess.py

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -9,7 +9,7 @@ import argparse
 import nibabel as nib
 
 def main(args):
-    modules = 'gcc/6.3.0 python/3.6 py-numpy/1.14.3_py36 py-pandas/0.23.0_py36 viz py-scikit-learn/0.19.1_py36 antspy/0.3.2'
+    modules = 'gcc/6.3.0 python/3.6 py-numpy/1.14.3_py36 py-pandas/0.23.0_py36 viz py-scikit-learn/0.19.1_py36 antspy/0.2.2'
     
     #########################
     ### Setup preferences ###
